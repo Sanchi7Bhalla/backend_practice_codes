08fe87e8ff0eac14bbbfc5ef0f381a9bf72efd43
sanchi <sanchibhalla98@gmail.com>
2021-01-09 17:22:59 +0530

    practice_5

--- hello/server.js
+++ hello/server.js
@@ -2,7 +2,6 @@ var http=require('http');
 var dt = require('./dateTime');
 
 http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type': 'text/html'});
     res.write('Hello World!! ');
     res.write('date and time is :'+dt.myDateTime());
     res.end();
